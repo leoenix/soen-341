23ad98c21b7989648dd1c65edd09e61061771386
Nadine El-Mufti <nadine.el.mufti@gmail.com>
2021-11-07 04:58:53 -0500

    Attempt to solve #124 Update DB when voting buttons are pressed

--- src/SpecificQuestionPage.js
+++ src/SpecificQuestionPage.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 import Header1 from "./Header1";
 import styled from "styled-components";
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import DarkCyanButton from './DarkCyanButton'
 import VotingComponent from './VotingComponent'
 import VotingArrows from "./VotingArrows";
 import TopQuestionsHeader from "./Header1";
 import { config } from "@fortawesome/fontawesome-svg-core";
 
-const Box = styled.div`
+const Box = styled.div `
   padding: 40px 20px;
   display: flex;
   flex-direction: column`
-const titlee = styled.div`
+const titlee = styled.div `
   padding-bottom: 40px;`
-const QuestionBodyTextArea = styled.textarea`
+const QuestionBodyTextArea = styled.textarea `
   background: none;
   border: 1px solid #aaa;
   border-radius: 3px;
@@ -41,15 +41,16 @@ const CheckBestAnswer = styled.div `
 
 function SpecificQuestionPage(props) {
     const [specificQuestion, setSpecificQuestion] = useState(false);
-    const [vote, setVote] = useState(0);
-    const AnswerHeader = styled.h2`font-size: 1.5rem;
+    const [voteCount, setVoteCount] = useState(0);
+    const AnswerHeader = styled.h2 `font-size: 1.5rem;
       color: black;`
     const [answers, setAnswers] = useState([]);
     const [theAnswer, setTheAnswer] = useState('');
     console.log(specificQuestion);
     const questionid = props.match.params.questionid;
     const [info, setInfo] = useState(false);
-    const [user,setUser] = useState('');
+    const [user, setUser] = useState('');
+
     function getQuestion() {
         axios.get('http://localhost:3030/question/' + questionid).then(res => {
             setInfo(res.data);
@@ -66,7 +67,7 @@ function SpecificQuestionPage(props) {
         axios.post('http://localhost:3030/postanswer', {
             description: theAnswer,
             questionid: specificQuestion.questionid
-        }, {withCredentials: true}).then(res => {
+        }, { withCredentials: true }).then(res => {
 
             setTheAnswer('');
             window.location.reload();
@@ -79,126 +80,158 @@ function SpecificQuestionPage(props) {
         axios.get('http://localhost:3030/question/' + questionid).then(res => {
             setInfo(res.data);
             setSpecificQuestion(res.data);
-
+            setVoteCount(res.data.question.vote_count)
         });
     }
 
-    console.log('length is ' + answers
-    );
+    console.log('length is ' + answers);
+
     function getAnswers() {
-        axios.get('http://localhost:3030/answers/' + questionid, {withCredentials:true}).then(res => {
+        axios.get('http://localhost:3030/answers/' + questionid, { withCredentials: true }).then(res => {
             setAnswers(res.data);
-        });}
+        });
+    }
 
 
 
     function getUser() {
-            axios.get('http://localhost:3030/user', {withCredentials: true})
-                .then(response => {
-                    console.log('here')
-                    console.log(response.data.userid)
-                    setUser(response.data.userid);
-                    console.log('here again')
+        axios.get('http://localhost:3030/user', { withCredentials: true })
+            .then(response => {
+                console.log('here')
+                console.log(response.data.userid)
+                setUser(response.data.userid);
+                console.log('here again')
 
-                })
-                .catch(() => {
-                    setUser(null);
-                })
+            })
+            .catch(() => {
+                setUser(null);
+            })
     }
 
-    function selectBestAnswer(answerid, bestanswer){
-        if (bestanswer === 1){
-            axios.put('http://localhost:3030/removebestanswer/' + questionid + '/' + answerid, {withCredentials: true})
+    function selectBestAnswer(answerid, bestanswer) {
+        if (bestanswer === 1) {
+            axios.put('http://localhost:3030/removebestanswer/' + questionid + '/' + answerid, { withCredentials: true })
                 .then(response => {
 
                     window.location.reload();
                 })
                 .catch(() => {
                     console.log('some error happened');
-                })}
+                })
+        } else {
+            axios.put('http://localhost:3030/bestanswer/' + questionid + '/' + answerid, { withCredentials: true })
+                .then(response => {
 
-        else {
-        axios.put('http://localhost:3030/bestanswer/' + questionid + '/' + answerid, {withCredentials: true})
-            .then(response => {
-
-            window.location.reload();
-            })
-            .catch(() => {
-                console.log('some error happened');
-            })}
+                    window.location.reload();
+                })
+                .catch(() => {
+                    console.log('some error happened');
+                })
+        }
     }
 
-    function handleOnUpArrowClick(){
-        axios.post('http://localhost:3030/vote/up/' + questionid, {withCredentials: true})
-        .then(currentVoteNumber => setVote(currentVoteNumber))
-
-    }
-
-
-    function handleOnDownArrowClick(){
-        axios.post('http://localhost:3030/vote/down/' + questionid, {withCredentials: true})
-        .then(currentVoteNumber => setVote(currentVoteNumber))
+    function handleOnUpArrowClick() {
+        axios.post('http://localhost:3030/vote/up/' + questionid, { withCredentials: true })
+        .then(response => setVoteCount(response.data))
     }
 
 
-//disabled = {specificQuestion.userid = user.userid}
+    function handleOnDownArrowClick() {
+        axios.post('http://localhost:3030/vote/down/' + questionid, { withCredentials: true })
+            .then(response => setVoteCount(response.data))
+    }
 
 
-        useEffect(() => {
-            getQuestion();
-            getAnswers();
-            getUser();
-        }, []);
-
-        return (
-            <>
-
-                <Box>
-                    <div><Header1>{info && info.title} </Header1></div>
-                    <hr style={{borderColor: 'lightgrey', width: '-webkit-fill-available'}}></hr>
-                    <div style = {{display:'flex', justifyContent:'space-between'}}><div style={{color: 'black', display: 'flex'}}>
-                         <VotingArrows style={{marginTop: '10px'}} value={vote}
-                         vote={1}
-                         onUpArrowClick={ () => handleOnUpArrowClick() } 
-                         onDownArrowClick={ () => handleOnDownArrowClick() } />
-                        <div><div children = {info.description} style ={{color:'black', padding: "15px 40px"}}></div>  </div>
-
-                    </div>
-
-                        <span style = {{alignSelf: 'flex-end', color:'black'}} children={specificQuestion.email}>asked by {specificQuestion.email} </span>
-                    </div>
+    //disabled = {specificQuestion.userid = user.userid}
 
 
-                    <Header1 style ={{margin:'20px 0px'}}>Answers </Header1>
-                    {answers && answers.length > 0 && answers.map(a => (<>
+    useEffect(() => {
+        getQuestion();
+        getAnswers();
+        getUser();
+    }, []);
 
-                            <hr style={{borderColor: 'lightgrey', width: '-webkit-fill-available'}}></hr>
-                            <div style = {{display:'flex', justifyContent:'space-between'}}><div style={{color: 'black', display: 'flex'}}> <div> <VotingComponent value = {1} vote = {1}></VotingComponent>
-                                <CheckBestAnswer valid ={a.bestanswer === 1} disabled = {specificQuestion.userid !== user} onClick = {() => selectBestAnswer(a.answerid, a.bestanswer)} ></CheckBestAnswer> </div>
-                                <div children = {a.description} style ={{color:'black', padding: "15px 40px"}}></div>
+    return ( <>
 
-                            </div>
+        <Box>
+        <div> <Header1> { info && info.title } </Header1></div>
+        <hr style = {
+            { borderColor: 'lightgrey', width: '-webkit-fill-available' }} > </hr> <div style = {
+            { display: 'flex', justifyContent: 'space-between' }} > <div style = {
+            { color: 'black', display: 'flex' }} >
+        <VotingArrows style = {{ marginTop: '10px' }}
+        value = { voteCount }
+        vote = { 1 }
+        onUpArrowClick = {
+            () => handleOnUpArrowClick()
+        }
+        onDownArrowClick = {
+            () => handleOnDownArrowClick()
+        }
+        /> <div > < div children = { info.description }
+        style = {
+            { color: 'black', padding: "15px 40px" }
+        } > </div>  </div >
 
-                     <span style = {{alignSelf: 'flex-end', color:'black'}}children={a.email}>          asked by {a.email} </span>
-                            </div>
+        </div>
 
-                        </>
-                    ))}
-
-
-                    <hr style={{borderColor: 'lightgrey', width: '-webkit-fill-available'}}></hr>
+        <span style = {
+            { alignSelf: 'flex-end', color: 'black' }
+        }
+        children = { specificQuestion.email } > asked by { specificQuestion.email } </span> </div>
 
 
-                    <AnswerHeader style={{margin: '10px 0px 10px'}}>Your answer</AnswerHeader>
+        <Header1 style = {{ margin: '20px 0px' }} > Answers </Header1> {
+        answers && answers.length > 0 && answers.map(a => ( < >
 
-                    <QuestionBodyTextArea value={theAnswer} onChange={ev => setTheAnswer(ev.target.value)}
-                                          placeholder="Please type in your answer in detail."/>
-                    <DarkCyanButton style={{width: 'fit-content'}} type={'submit'} onClick={ev => postAnswer(ev)}>Post
-                        answer</DarkCyanButton>
-                </Box>
+            <hr style = {
+                { borderColor: 'lightgrey', width: '-webkit-fill-available' }} > </hr> 
+                <div style = {{ display: 'flex', justifyContent: 'space-between' }}> 
+                <div style = {
+                { color: 'black', display: 'flex' }}> 
+                <div> 
+                <VotingComponent value = { 1 }
+                                 vote = { 1 } >
+                </VotingComponent> 
+            <CheckBestAnswer valid = { a.bestanswer === 1 }
+                             disabled = { specificQuestion.userid !== user }
+                             onClick = { () => selectBestAnswer(a.answerid, a.bestanswer)} > 
+            </CheckBestAnswer> </div >
+            <div children = { a.description }
+            style = {
+                { color: 'black', padding: "15px 40px" }} > </div>
+
+            </div>
+
+            <span style = {
+                { alignSelf: 'flex-end', color: 'black' }
+            }
+            children = { a.email } > asked by { a.email } </span> </div >
+
             </>
-        );
+        ))
     }
 
 
-    export default SpecificQuestionPage;
+    <hr style = {
+        { borderColor: 'lightgrey', width: '-webkit-fill-available' }}> </hr>
+
+
+    <AnswerHeader style = {
+        { margin: '10px 0px 10px' }
+    } > Your answer </AnswerHeader>
+
+    <
+    QuestionBodyTextArea value = { theAnswer }
+    onChange = { ev => setTheAnswer(ev.target.value) }
+    placeholder = "Please type in your answer in detail." / >
+        <DarkCyanButton style = {{ width: 'fit-content' }}
+                        type = { 'submit' }
+                        onClick = { ev => postAnswer(ev) } > Post answer </DarkCyanButton> 
+    </Box> 
+    </>
+);
+}
+
+
+export default SpecificQuestionPage;
